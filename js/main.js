4afa44cc5ff6d1b71609bbb1a0c36fb13b64a5f1
dziubacaique <caiquedziuba@hotmail.com>
2021-05-21 19:10:44 -0300

    Operacoes basicas javascript

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/* Caique M Dziuba */ 
+// Maio de 2021
+
+var nome = "Caique Dziuba";
+var idade = 28;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo.";
+alert(nome + " tem " + idade + " anos");
+
+
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.replace("Japão", "Brasil")); //substitui uma palavra da string por outra
+alert(frase.replace("Japão", "Brasil")); //substitui uma palavra da string por outra
+console.log(frase.toUpperCase()); //Deixa a frase toda maiuscula
+console.log(frase.toLowerCase()); //Deixa a frase toda minuscula
