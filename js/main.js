34d8ceedef9a580bdf32354121736b67d3959d1e
dziubacaique <caiquedziuba@hotmail.com>
2021-05-21 19:20:11 -0300

    lista - vetor

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 /* Caique M Dziuba */ 
 // Maio de 2021
 
+var lista = ["maça", "pêra", "laranja"]; //lista
+lista.push("uva"); //incluir elemento na lista
+lista.pop(); //remove ultimo elemento da lista
+console.log(lista[1]); //exibir item de posição 1 na lista
+console.log(lista); //exibe lista completa
+console.log(lista.length); //exibe tamanho da lista
+console.log(lista.reverse()); //metodo que reverte lista
+console.log(lista.toString()); //transforma o array em string
+console.log(lista.toString()[0]); //exibe apenas primeiro elemnto DA STRING
+console.log(lista.join(" - ")); //substitui a virgula por outro elemento para separar as strings
+
+
+/*
 var nome = "Caique Dziuba";
 var idade = 28;
 var idade2 = 10;
@@ -14,3 +27,4 @@ console.log(frase.replace("Japão", "Brasil")); //substitui uma palavra da strin
 alert(frase.replace("Japão", "Brasil")); //substitui uma palavra da string por outra
 console.log(frase.toUpperCase()); //Deixa a frase toda maiuscula
 console.log(frase.toLowerCase()); //Deixa a frase toda minuscula
+*/
